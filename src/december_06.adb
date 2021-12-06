41c011a2ae1bec6397b8022775adec3438e36a69
David-Haley <david.haley@bigpond.com>
2021-12-06 23:05:02 +1000

    Update december_06.adb
    Added comments with respect to population distribution, corrected some spelling and put the input reading code in a separate procedure. I will follow this pattern for future days.

--- src/december_06.adb
+++ src/december_06.adb
@@ -10,13 +10,51 @@ procedure December_06 is
    subtype First_Cycles is Positive range 1 .. 2;
    type Adult_Cycles is mod 7;
 
-   type Juvanile_Populations is array (First_Cycles) of Unsigned_64;
+   type Juvenile_Populations is array (First_Cycles) of Unsigned_64;
    type Adult_Populations is array (Adult_Cycles) of Unsigned_64;
 
    type Schools is record
-      Juvanile_Population : Juvanile_Populations := (others => 0);
+      Juvenile_Population : Juvenile_Populations := (others => 0);
       Adult_Population : Adult_Populations := (others => 0);
    end record; -- Schools
+
+   procedure Get_Input (School : out Schools;
+                        Day_Limit : out Positive) is
+
+      -- december_06 [Input_File_Name [Part_One_Days]]
+      -- Note there is something seriously wrong with this population the "day
+      -- ages" of all the fish in my input were only in the adult range. There
+      -- are no juvenile fish! If someone has input which has some juvenile
+      -- fish a constraint exception will be raised.
+
+      Input_File : File_Type;
+      Text : Unbounded_String;
+      Start_At, First : Positive;
+      Last : Natural;
+      Adult_Cycle : Adult_Cycles;
+
+   begin -- Get_Input
+      if Argument_Count = 0 then
+         Open (Input_File, In_File, "december_06.txt");
+      else
+         Open (Input_File, In_File, Argument(1));
+      end if; -- Argument_Count = 0
+      if Argument_Count > 1 then
+         Day_Limit := Positive'Value (Argument (2));
+      else
+         Day_Limit := 80;
+      end if; -- Argument_Count > 1
+      Get_Line (Input_File, Text);
+      Start_At := 1;
+      while Start_At < Length (Text) loop
+         Find_Token (Text, Decimal_Digit_Set, Start_At, Inside, First, Last);
+         Adult_Cycle := Adult_Cycles'Value (Slice (Text, First, Last));
+         School.Adult_Population (Adult_Cycle) :=
+           School.Adult_Population (Adult_Cycle) + 1;
+         Start_At := Last + 1;
+      end loop; -- Start_At < Length (Text)
+      Close (Input_File);
+   end Get_Input;
 
    procedure Update (School : in out Schools) is
 
@@ -31,9 +69,9 @@ procedure December_06 is
       end loop; -- A in Adult_Cycles
       New_School.Adult_Population (Adult_Cycles'Last) :=
         New_School.Adult_Population (Adult_Cycles'Last) +
-        School.Juvanile_Population (1);
-      New_School.Juvanile_Population (1) := School.Juvanile_Population (2);
-      New_School.Juvanile_Population (2) := Hatchling_Count;
+        School.Juvenile_Population (1);
+      New_School.Juvenile_Population (1) := School.Juvenile_Population (2);
+      New_School.Juvenile_Population (2) := Hatchling_Count;
       School := New_School;
    end Update;
 
@@ -46,39 +84,17 @@ procedure December_06 is
          Total := Total + School.Adult_Population (A);
       end loop; -- A in Adult_Cycles
       for J in First_Cycles loop
-         Total := Total + School.Juvanile_Population (J);
+         Total := Total + School.Juvenile_Population (J);
       end loop; -- J in First_Cycles
       return Total;
    end Population_Count;
 
-   Input_File : File_Type;
-   Text : Unbounded_String;
-   Start_At, First : Positive;
-   Last : Natural;
    School : Schools;
-   Adult_Cycle : Adult_Cycles;
-   Day_Limit : Positive := 80;
+   Day_Limit : Positive;
    Day : Positive := 1;
 
 begin -- December_06
-   if Argument_Count = 0 then
-      Open (Input_File, In_File, "december_06.txt");
-   else
-      Open (Input_File, In_File, Argument(1));
-      if Argument_Count > 1 then
-         Day_Limit := Positive'Value (Argument (2));
-      end if; -- Argument_Count > 1
-   end if; -- Argument_Count = 0
-   Get_Line (Input_File, Text);
-   Start_At := 1;
-   while Start_At < Length (Text) loop
-      Find_Token (Text, Decimal_Digit_Set, Start_At, Inside, First, Last);
-      Adult_Cycle := Adult_Cycles'Value (Slice (Text, First, Last));
-      School.Adult_Population (Adult_Cycle) :=
-        School.Adult_Population (Adult_Cycle) + 1;
-      Start_At := Last + 1;
-   end loop; -- Start_At < Length (Text)
-   Close (Input_File);
+   Get_Input (School, Day_Limit);
    While Day <= Day_Limit loop
       Update (School);
       Day := Day + 1;
@@ -87,6 +103,6 @@ begin -- December_06
    while Day <= 256 loop
       Update (School);
       Day := Day + 1;
-   end loop; -- D in Positive range 1 .. Day_Limit
+   end loop; -- Day <= 256
    Put_Line ("Part Two Answer:" & Population_Count (School)'Img);
 end December_06;
