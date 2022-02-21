979e3ed4f22c94fbb7ee80acc0035cb06d2d036c
David-Haley <34273510+David-Haley@users.noreply.github.com>
2022-02-21 10:35:26 +1000

    Update december_19.adb
    Solves both parts slowly!

--- src/december_19.adb
+++ src/december_19.adb
@@ -9,7 +9,7 @@ with Ada.Containers; use Ada.Containers;
 with Ada.Containers.Vectors;
 with Ada.Containers.Ordered_Sets;
 with Ada.Containers.Synchronized_Queue_Interfaces;
-with Ada.Containers.Unbounded_Synchronized_Queues;
+with Ada.Containers.Unbounded_Priority_Queues;
 with DJH.Execution_Time;
 
 procedure December_19 is
@@ -43,57 +43,24 @@ procedure December_19 is
      Ada.Containers.Vectors (Scanner_Indices, Scanners);
    use Scanner_Lists;
 
-   procedure Get_Input (Scanner_List : out Scanner_Lists.Vector) is
+   function "<" (Left, Right : Coordinates) return Boolean is
 
-      -- december_19 [Input_File_Name]
+   begin -- "<"
+      return Left (X_Axis) < Right (X_Axis)
+        or
+          (Left (X_Axis) = Right (X_Axis) and (Left (Y_Axis) < Right (Y_Axis)
+                                               or
+                                                 (Left (Y_Axis) = Right (Y_Axis) and
+                                                      Left (Z_Axis) < Right (Z_Axis))));
+   end "<";
 
-      Input_File : File_Type;
-      Text : Unbounded_String;
-      Scanner_String : constant String := "--- scanner";
-      Integer_Set : constant Character_Set := To_Set ("-0123456789");
-      Start_At, First : Positive;
-      Last : Natural;
-      Scanner_Index : Scanner_Indices := 0;
-      Scanner : Scanners;
-      Beacon : Coordinates;
+   package Beacon_Sets is new Ada.Containers.Ordered_Sets (Coordinates);
+   use Beacon_Sets;
 
-   begin -- Get_Input
-      if Argument_Count = 0 then
-         Open (Input_File, In_File, "december_19.txt");
-      else
-         Open (Input_File, In_File, Argument(1));
-      end if; -- Argument_Count = 0
-      while not End_Of_File (Input_File) loop
-         Get_Line (Input_File, Text);
-         if Length (Text) = 0 then
-            Append (Scanner_List, Scanner);
-            Scanner.Beacon_Array (R0) := Beacon_Lists.Empty_Vector;
-         elsif Index (Text, Scanner_String) /= 0 then
-            Start_At := Scanner_String'Length + Index (Text, Scanner_String);
-            Find_Token (Text, Decimal_Digit_Set, Start_At, Inside, First, Last);
-            Scanner_Index := Scanner_Indices'Value (Slice (Text, First, Last));
-            Scanner.Position_Known := Scanner_Index = Scanner_Indices'First;
-         else
-            Start_At := 1;
-            for A in Axies loop
-               Find_Token (Text, Integer_Set, Start_At, Inside, First, Last);
-               Beacon (A) := Offsets'Value (Slice (Text, First, Last));
-               Start_At := Last + 1;
-            end loop; -- A in Axies
-            Append (Scanner.Beacon_Array (R0), Beacon);
-         end if; -- Length (Text) = 0
-      end loop; -- End_Of_File (Input_File)
-      -- Save last Scanner if there was no blank line before EOF
-      if Length (Scanner.Beacon_Array (R0)) > 0 then
-         Append (Scanner_List, Scanner);
-      end if; -- Length (Scanner.Beacon_Array (R0)) > 0
-      Close (Input_File);
-   end Get_Input;
-
-   procedure Build_Rotations (Scanner_List : in out Scanner_Lists.Vector) is
+   procedure Build_Rotations (Scanner : in out Scanners) is
 
       function Rotate (Coordinate : in Coordinates;
-                      Rotation : in Rotations) return Coordinates is
+                       Rotation : in Rotations) return Coordinates is
 
          type Rotation_Matrix is Array (Axies, Axies) of Rotation_Elements;
 
@@ -113,7 +80,7 @@ procedure December_19 is
                    ( 0, -1,  0)),
             04 => (( 0, -1,  0),
                    ( 1,  0,  0),
-                   ( 0,  0, -1)),
+                   ( 0,  0,  1)),
             05 => (( 0,  0,  1),
                    ( 1,  0,  0),
                    ( 0,  1,  0)),
@@ -188,15 +155,63 @@ procedure December_19 is
       end Rotate;
 
    begin -- Build_Rotations
-      for S in Iterate (Scanner_List) loop
-         for B in Iterate (Scanner_List (S).Beacon_Array (R0)) loop
-            for R in Rotations range R0 + 1 .. Rotations'Last  loop
-               Append (Scanner_List (S).Beacon_Array (R),
-                 Rotate (Scanner_List (S).Beacon_Array (R0) (B), R));
-            end loop; -- R in Rotations range R0 + 1 .. Rotations'Last
-         end loop; -- B in Iterate (Scanner_List (S).Beacon_Array (R0))
-      end loop; -- S in Iterate (Scanner_List)
+      for R in Rotations range R0 + 1 .. Rotations'Last  loop
+         Scanner.Beacon_Array (R) := Beacon_Lists.Empty_Vector;
+         for B in Iterate (Scanner.Beacon_Array (R0)) loop
+            Append (Scanner.Beacon_Array (R),
+                    Rotate (Scanner.Beacon_Array (R0) (B), R));
+         end loop; -- B in Iterate (Scanner.Beacon_Array (R0))
+      end loop; -- R in Rotations range R0 + 1 .. Rotations'Last
    end Build_Rotations;
+
+   procedure Get_Input (Scanner_List : out Scanner_Lists.Vector) is
+
+      -- december_19 [Input_File_Name]
+
+      Input_File : File_Type;
+      Text : Unbounded_String;
+      Scanner_String : constant String := "--- scanner";
+      Integer_Set : constant Character_Set := To_Set ("-0123456789");
+      Start_At, First : Positive;
+      Last : Natural;
+      Scanner_Index : Scanner_Indices := 0;
+      Scanner : Scanners;
+      Beacon : Coordinates;
+
+   begin -- Get_Input
+      if Argument_Count = 0 then
+         Open (Input_File, In_File, "december_19.txt");
+      else
+         Open (Input_File, In_File, Argument(1));
+      end if; -- Argument_Count = 0
+      while not End_Of_File (Input_File) loop
+         Get_Line (Input_File, Text);
+         if Length (Text) = 0 then
+            Build_Rotations (Scanner);
+            Append (Scanner_List, Scanner);
+            Scanner.Beacon_Array (R0) := Beacon_Lists.Empty_Vector;
+         elsif Index (Text, Scanner_String) /= 0 then
+            Start_At := Scanner_String'Length + Index (Text, Scanner_String);
+            Find_Token (Text, Decimal_Digit_Set, Start_At, Inside, First, Last);
+            Scanner_Index := Scanner_Indices'Value (Slice (Text, First, Last));
+            Scanner.Position_Known := Scanner_Index = Scanner_Indices'First;
+         else
+            Start_At := 1;
+            for A in Axies loop
+               Find_Token (Text, Integer_Set, Start_At, Inside, First, Last);
+               Beacon (A) := Offsets'Value (Slice (Text, First, Last));
+               Start_At := Last + 1;
+            end loop; -- A in Axies
+            Append (Scanner.Beacon_Array (R0), Beacon);
+         end if; -- Length (Text) = 0
+      end loop; -- End_Of_File (Input_File)
+      -- Save last Scanner if there was no blank line before EOF
+      if Length (Scanner.Beacon_Array (R0)) > 0 then
+         Build_Rotations (Scanner);
+         Append (Scanner_List, Scanner);
+      end if; -- Length (Scanner.Beacon_Array (R0)) > 0
+      Close (Input_File);
+   end Get_Input;
 
    function "+" (Left, Right : Coordinates) return Coordinates is
 
@@ -216,17 +231,38 @@ procedure December_19 is
 
    procedure Find_Matches (Scanner_List : in out Scanner_Lists.Vector) is
 
-      package QI is new
-        Ada.Containers.Synchronized_Queue_Interfaces (Scanner_Indices);
+      type Queue_Elements is record
+         Scanner_Index : Scanner_Indices;
+         Position : Coordinates;
+      end record; -- Queue_Elements
 
-      package Queues is new Ada.Containers.Unbounded_Synchronized_Queues (QI);
+      function Get_Priority (Queue_Element : Queue_Elements) return Natural is
+
+      begin -- Get_Priority
+         return abs (Queue_Element.Position (X_Axis)) +
+         abs (Queue_Element.Position (Y_Axis)) +
+         abs (Queue_Element.Position (Z_Axis));
+      end Get_Priority;
+
+      function Before (Left, Right : Natural) return Boolean is
+
+      begin -- Before
+         return Left < Right;
+      end Before;
+
+      package QI is new
+        Ada.Containers.Synchronized_Queue_Interfaces (Queue_Elements);
+
+      package Queues is new
+        Ada.Containers.Unbounded_Priority_Queues (QI,
+                                                  Queue_Priority => Natural);
       use Queues;
 
       procedure Match_Pair (Known : in Scanners;
                             Unknown : in out Scanners) is
 
          function Count_Matches (Known, Unknown : in Beacon_Lists.Vector)
-                              return natural is
+                                 return natural is
 
             package Match_Sets is new
               Ada.Containers.Ordered_Sets (Beacon_Indices);
@@ -292,46 +328,105 @@ procedure December_19 is
          end loop; -- Ru
       end Match_Pair;
 
-      function All_Matched (Scanner_List : in Scanner_Lists.Vector)
-                            return Boolean is
-
-         Result : Boolean := True;
-
-      begin -- All_Matched
-         for S in Iterate (Scanner_List) loop
-            Result := Result and Scanner_List (S).Position_Known;
-         end loop; -- S in Iterate (Scanner_List)
-         return Result;
-      end All_Matched;
-
-      Known_Index : Scanner_Indices := First_Index (Scanner_List);
+      Queue_In, Queue_Out : Queue_Elements;
       Known_Queue : Queues.Queue;
 
    begin -- Find_Matches
-      Known_Queue.Enqueue (Known_Index);
+      Queue_In.Scanner_Index := First_Index (Scanner_List);
+      Queue_In.Position :=
+        Scanner_List (Queue_In.Scanner_Index).Position;
+      Known_Queue.Enqueue (Queue_In);
       while Known_Queue.Current_Use > 0 loop
-         Known_Queue.Dequeue (Known_Index);
+         Known_Queue.Dequeue (Queue_Out);
          for Su in iterate (Scanner_List) loop
             if not Scanner_List (Su).Position_Known then
-               Match_Pair (Scanner_List (Known_Index), Scanner_list (Su));
-               Put_Line ("Comparing " & Known_Index'Img & To_Index (Su)'Img);
+               Match_Pair (Scanner_List (Queue_Out.Scanner_Index),
+                           Scanner_list (Su));
                if Scanner_List (Su).Position_Known then
-                  Known_Queue.Enqueue (To_Index (Su));
-                  Put_Line (To_Index (Su)'Img & Scanner_List (Su).Position (X_Axis)'Img & Scanner_List (Su).Position (Y_Axis)'Img & Scanner_List (Su).Position (Z_Axis)'Img);
+                  Queue_In.Scanner_Index := To_Index (Su);
+                  Queue_In.Position :=
+                    Scanner_List (Queue_In.Scanner_Index).Position;
+                  Known_Queue.Enqueue (Queue_In);
                end if; -- Scanner_List (Sk).Known_Position and ...
             end if; -- not Scanner_List (Su).Position_Known
          end loop; -- Su in iterate (Scanner_List)
       end loop; -- Known_Queue.Current_Use
    end Find_Matches;
 
+   function All_Matched (Scanner_List : in Scanner_Lists.Vector)
+                         return Boolean is
+
+      Result : Boolean := True;
+
+   begin -- All_Matched
+      for S in Iterate (Scanner_List) loop
+         Result := Result and Scanner_List (S).Position_Known;
+      end loop; -- S in Iterate (Scanner_List)
+      return Result;
+   end All_Matched;
+
+   procedure Build_Beacon_Set (Scanner_List : in Scanner_Lists.Vector;
+                               Beacon_Set : out Beacon_Sets.Set) is
+
+      R : Rotations;
+
+   begin -- Build_Beacon_Set
+      for S in Iterate (Scanner_List) loop
+         R := Scanner_List (S).Orientation;
+         for B in Iterate (Scanner_List (S).Beacon_Array (R)) loop
+            Include (Beacon_Set, Scanner_List (S).Beacon_Array (R) (B)
+                     + Scanner_List (S).Position);
+         end loop; -- B in Iterate (Scanner_List (S).Beacon_Array (R))
+      end loop; -- S in Iterate (Scanner_List)
+   end Build_Beacon_Set;
+
+   function Maximum_Distance (Scanner_List : in Scanner_Lists.Vector)
+                              return Natural is
+
+      function Distance (Left, Right : in Coordinates) return Natural is
+
+         -- Manhattan distance
+
+      begin -- Distance
+         return abs (Left (X_Axis) - Right (X_Axis)) +
+         abs (Left (Y_Axis) - Right (Y_Axis)) +
+         abs (Left (Z_Axis) - Right (Z_Axis));
+      end Distance;
+
+      Result : Natural := Natural'First;
+
+   begin -- Maximum_Distance
+      for S1 in Scanner_Indices range Scanner_Indices'First ..
+        Last_Index (Scanner_List) - 1 loop
+         for S2 in Scanner_Indices range S1 + 1 ..
+           Last_Index (Scanner_List) loop
+            if Result < Distance (Scanner_List (S1).Position,
+                                  Scanner_List (S2).Position) then
+               Result := Distance (Scanner_List (S1).Position,
+                                   Scanner_List (S2).Position);
+            end if; -- Result < Distance (Scanner_List (S1).Position, ...
+         end loop; -- S2 in Scanner_Indices range S1 + 1 ...
+      end loop; -- S1 in Scanner_Indices range Scanner_Indices'First ..
+      return Result;
+   end Maximum_Distance;
+
    Scanner_List : Scanner_Lists.Vector;
+   Beacon_Set : Beacon_Sets. Set;
 
 begin -- December_19
    Get_Input (Scanner_List);
-   Build_Rotations (Scanner_List);
    Find_Matches (Scanner_List);
-   Put_Line ("Part One Answer:");
-   DJH.Execution_Time.Put_CPU_Time;
-   Put_Line ("Part Two Answer:");
-   DJH.Execution_Time.Put_CPU_Time;
+   if All_Matched (Scanner_List) then
+      Build_Beacon_Set (Scanner_List, Beacon_Set);
+      Put_Line ("Part One Answer:" & Length (Beacon_Set)'Img);
+      DJH.Execution_Time.Put_CPU_Time;
+      Put_Line ("Part Two Answer:" & Maximum_Distance (Scanner_List)'Img);
+      DJH.Execution_Time.Put_CPU_Time;
+   else
+      Put ("Unmatched scanners");
+      for S in Iterate (Scanner_List) loop
+         Put (To_Index (S)'Img);
+      end loop; -- S in Iterate (Scanner_Lists)
+      New_line;
+   end if;
 end December_19;
