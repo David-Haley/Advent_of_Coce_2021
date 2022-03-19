5b331d561705ec5cbb28223c876a3239d9578dba
David-Haley <david.haley@bigpond.com>
2022-03-19 13:04:08 +1000

    Day 24 part two complete, AOC 2021 complete!
    My biggest problem with Day 24 was the concept of universes. Until I looked at the Megatrend I did not appreciate that this equated to all possible outcomes (unique or otherwise). The one point I missed initially was tracking possible outcomes up to the a player wins. The solution here uses a queue to sore the state of all uncompleted game instances. No cashing is used and the run time for part two is about 10 s for the example. Interestingly the maximum number of game instances in the queue is 3946912 for the example. I got the queue idea from Megatrends; however when I went back I could not find the reference, I believe this is a reasonably elegant solution. The solution is now self contained, functionality of the deterministic die has been incorporated into the Next_Position procedure on part one.

--- src/december_21.adb
+++ src/december_21.adb
@@ -5,10 +5,8 @@ with Ada.Strings; use Ada.Strings;
 with Ada.Strings.Unbounded; use Ada.Strings.Unbounded;
 with Ada.Strings.Maps.Constants; use Ada.Strings.Maps.Constants;
 with Ada.Containers; use Ada.Containers;
-with Ada.Containers.Ordered_Maps;
 with Ada.Containers.Synchronized_Queue_Interfaces;
 with Ada.Containers.Unbounded_Synchronized_Queues;
-with Deterninistic_Die; use Deterninistic_Die;
 with Interfaces; use Interfaces;
 with DJH.Execution_Time;
 
@@ -25,19 +23,11 @@ procedure December_21 is
       Score : Scores := 0;
    end record; -- Players
 
-   subtype Universes is Unsigned_64;
-
    type Player_Arrays is array (Player_Indices) of Players;
 
-   type Player_Results is record
-      Wins, Losses : Universes := 0;
-   end record; -- Player_Results
-   type Results is array (Player_Indices) of Player_Results;
+   subtype Universes is Unsigned_64;
 
-   subtype Turns is Natural;
-
-   package Turn_Stores is new Ada.Containers.Ordered_Maps (Turns, Results);
-   use Turn_Stores;
+   type Results is array (Player_Indices) of Universes;
 
    procedure Get_Input (Player_Array : out Player_Arrays) is
 
@@ -71,15 +61,31 @@ procedure December_21 is
    function Part_One (Player_Array_In : in Player_Arrays) return Natural is
 
       End_Score : constant  Natural := 1000;
+      subtype Throws is Positive range 1 .. 100;
 
-      function Next_Position (Position : in Board_Positions;
-                              Throw : Die_Values)
-                              return Board_Positions is
+      type Die_States is record
+         Throw : Throws := Throws'First;
+         Throw_Count : Natural := 0;
+      end record; -- Die_States
+
+      procedure Next_Position (Position : in out Board_Positions;
+                               Die_State : in out Die_States) is
+
+         -- The die is deterministic the next die state is dependent on the
+         -- previous state, hence state variable Die_State.
 
       begin -- Next_Position
-         return (Position + Throw - 1) mod 10 + 1;
+         Position :=
+           (Position + Die_State.Throw - 1) mod Board_Positions'Last + 1;
+         if Die_State.Throw < Throws'Last then
+            Die_State.Throw := Die_State.Throw + 1;
+         else
+            Die_State.Throw := Throws'First;
+         end if; -- Die_State.Throw < Throws'Last
+         Die_State.Throw_Count := Die_State.Throw_Count + 1;
       end Next_Position;
 
+      Die_State : Die_States;
       Player_Array : Player_Arrays := Player_Array_In;
       Game_Over : Boolean := False;
       Result : Natural := 0;
@@ -87,10 +93,9 @@ procedure December_21 is
    begin -- Part_One
       while not Game_Over loop
          for P in Player_Indices loop
-            if not  Game_Over then
+            if not Game_Over then
                for T in Positive range 1 .. 3 loop
-                  Player_Array (P).Board_Position :=
-                    Next_Position (Player_Array (P).Board_Position, Roll_Die);
+                    Next_Position (Player_Array (P).Board_Position, Die_State);
                end loop; -- T in Positive range 1 .. 3
                Player_Array (P).Score := Player_Array (P).Score +
                  Player_Array (P).Board_Position;
@@ -99,32 +104,38 @@ procedure December_21 is
          end loop; -- P in Player_Indices
       end loop; -- not Game_Over
       if Player_Array (1).Score >= End_Score then
-         Result := Die_Throws * Player_Array (2).Score;
+         Result := Die_State.Throw_Count * Player_Array (2).Score;
       elsif Player_Array (2).Score >= End_Score then
-         Result := Die_Throws * Player_Array (2).Score;
+         Result := Die_State.Throw_Count * Player_Array (1).Score;
       end if; -- Player_Array (P).Score >= End_Score
       return Result;
    end Part_One;
 
-   procedure Part_Two_Play (Player_Array : in Player_Arrays;
-                           Turn_Store : out Turn_Stores.Map) is
+   procedure Part_Two_Play (Player_Array_In : in Player_Arrays;
+                            Result : out Results) is
+
+      -- Rather than directly playing the game the frequency of outcomes is
+      -- calculated. For each outcome of three die rolls the frequency of that
+      -- outcome is calculated as the product of the frequency of the previous
+      -- state times the frequency of that roll total.
 
       Winning_Score : constant Scores := 21;
 
-      -- for three throws of the Dirac Dice there are 27 possible outcomes
+      -- For three throws of the Dirac Dice there are 27 possible outcomes
       -- ranging from 3 to 9 with frequencies as per the array below
+      subtype Throw_Totals is Positive range 3 .. 9;
+
       subtype Frequencies is Unsigned_64 with
         Static_Predicate => Frequencies in 1 | 3 | 6 | 7;
-      subtype Throw_Totals is Positive range 3 .. 9;
+
       type Dirac_Dice is array (Throw_Totals) of Frequencies;
 
       Dirac_Die : constant Dirac_Dice := (1, 3, 6, 7, 6, 3, 1);
 
       type States is record
-         Turn : Turns;
-         Board_Position : Board_Positions;
-         Score : Natural;
-         Score_Frequency : Universes;
+         Player_Array : Player_Arrays := Player_Array_In;
+         Frequency : Universes := 1; -- One instance of start of game
+         Turn : Player_Indices := 1; -- Player one plays first
       end record; -- States
 
       package Quantum_Queue_Interface is new
@@ -132,88 +143,63 @@ procedure December_21 is
 
       package Quantum_Queues is new
         Ada.Containers.Unbounded_Synchronized_Queues (Quantum_Queue_Interface);
-      use Quantum_Queues;
 
       function Next_Position (Board_Position : in Board_Positions;
                               Throw_Total : Throw_Totals)
                               return Board_Positions is
 
       begin -- Next_Position
-         return (Board_Position + Throw_Total - 1) mod 10 + 1;
+         return (Board_Position + Throw_Total - 1) mod Board_Positions'Last + 1;
       end Next_Position;
 
       Quantum_Queue : Quantum_Queues.Queue;
       State, Next_State : States;
-      Result : constant Results := ((0, 0), (0, 0));
 
    begin -- Part_Two_Play
-      for P in Player_Indices loop
-         Put_Line ("Player:" & P'Img);
-         State.Turn := 0;
-         State.Board_Position := Player_Array (P).Board_Position;
-         State.Score := 0;
-         State.Score_Frequency := 1;
-         Quantum_Queue.Enqueue (State);
-         while Quantum_Queue.Current_Use > 0 loop
-            Quantum_Queue.Dequeue (State);
-            if State.Score < Winning_Score then
-               if not Contains (Turn_Store, State.Turn) then
-                  Include (Turn_Store, State.Turn, Result);
-               end if; -- not Contains (Turn_Store, State.Turn)
-               Turn_Store (State.Turn) (P).Losses :=
-                 Turn_Store (State.Turn) (P).Losses + State.Score_Frequency;
-               Next_State.Turn := State.Turn + 1;
-               for Throw_Total in Throw_Totals loop
-                  Next_State.Board_Position :=
-                    Next_Position (State.Board_Position, Throw_Total);
-                  Next_State.Score := State.Score + Next_State.Board_Position;
-                  Next_State.Score_Frequency := State.Score_Frequency *
-                    Dirac_Die (Throw_Total);
-                  Quantum_Queue.Enqueue (Next_State);
-               end loop;
+      Result := (others => 0);
+      Quantum_Queue.Enqueue (State);
+      while Quantum_Queue.Current_Use > 0 loop
+         Quantum_Queue.Dequeue (State);
+         if State.Turn = 1 then
+            Next_State.Turn := 2;
+         else
+            Next_State.Turn := 1;
+         end if; -- State.Turn = 1
+         Next_State.Player_Array (Next_State.Turn) :=
+           State.Player_Array (Next_State.Turn);
+         -- transfer state of player who is not throwing die
+         for Throw_Total in Throw_Totals loop
+            Next_State.Player_Array (State.Turn).Board_Position :=
+              Next_Position (State.Player_Array (State.Turn).Board_Position,
+                             Throw_Total);
+            Next_State.Player_Array (State.Turn).Score :=
+              State.Player_Array (State.Turn).Score +
+              Next_State.Player_Array (State.Turn).Board_Position;
+            Next_State.Frequency := State.Frequency * Dirac_Die (Throw_Total);
+            if Next_State.Player_Array (State.Turn).Score < Winning_Score then
+               -- Continue playing this game instance
+               Quantum_Queue.Enqueue (Next_State);
             else
-               if not Contains (Turn_Store, State.Turn) then
-                  Include (Turn_Store, State.Turn, Result);
-               end if; -- not Contains (Turn_Store, State.Turn)
-               Turn_Store (State.Turn) (P).Wins :=
-                 Turn_Store (State.Turn) (P).Wins + State.Score_Frequency;
-            end if; -- State.Score < Winning_Score
-         end loop; --while Quantum_Queue.Current_Use > 0
-      end loop; -- P in Player_Indices
+               -- This gane instance is over, add the frequency of this outcome
+               -- to the winning player's win count.
+               Result (State.Turn) :=
+                 Result (State.Turn) + Next_State.Frequency;
+            end if; -- Next_State.Player_Array (State.Turn).Score ...
+         end loop; -- Throw_Total in Throw_Totals
+      end loop; --while Quantum_Queue.Current_Use > 0
+      Put_Line ("Maximum games in play (queue usage):" &
+                  Quantum_Queue.Peak_Use'Img);
    end Part_Two_Play;
 
-   function Part_Two (Turn_Store : in Turn_Stores.Map) return Universes is
-
-      Win_Universe : array (Player_Indices) of Universes := (others => 0);
-      Total : Results;
-
-   begin -- Part_Two
-      for T in Iterate (Turn_Store) loop
-         Put_Line (Key (T)'Img & Turn_Store (T) (1).Wins'Img & Turn_Store (T) (1).Losses'Img & Turn_Store (T) (2).Wins'Img & Turn_Store (T) (2).Losses'Img);
-         if Turn_Store (T) (1).Wins > 0 or  Turn_Store (T) (2).Wins > 0 then
-            for P in Player_Indices loop
-               Total (P).Wins := Total (P).Wins + Turn_Store (T) (P).Wins;
-               Total (P).Losses :=
-                 Total (P).Losses + Turn_Store (T) (P).Losses;
-            end loop; -- P in Player_Indices
-         end if; -- Turn_Store (T) (1).Wins > 0 or  Turn_Store (T) (2).Wins > 0
-      end loop; -- T in Iterate (Turn_Store)
-      Put_Line (Total (1).Wins'Img & Total (1).Losses'Img & Total (2).Wins'Img & Total (2).Losses'Img);
-      if Total (1).Wins * Total (2).Losses > Total (2).Wins * Total (1).Losses then
-         return Total (1).Wins * Total (2).Losses;
-      else
-         return Total (2).Wins * Total (1).Losses;
-      end if; -- Total.Wins (1) * Total.Losses (2) > Total.Wins (2) * Total.Losses (1)
-   end Part_Two;
-
-
    Player_Array : Player_Arrays;
-   Turn_Store : Turn_Stores.Map;
+   Result : Results;
 
 begin -- December_21
    Get_Input (Player_Array);
    Put_Line ("Part One Answer:" & Part_One (Player_Array)'Img);
-   Part_Two_Play (Player_Array, Turn_Store);
-   Put_Line ("Part Two Answer:" & Part_Two (Turn_Store)'Img);
+   DJH.Execution_Time.Put_CPU_Time;
+   Part_Two_Play (Player_Array, Result);
+   Put_Line ("Part Two Answer player One wins:" & Result (1)'Img &
+            " player two wins:" & Result (2)'Img);
    DJH.Execution_Time.Put_CPU_Time;
 end December_21;
